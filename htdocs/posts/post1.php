5e7b058cb79be8563500c486e03bb3e63f557a3e
ahmadansari <designixcreative@gmail.com>
2025-04-14 21:14:15 +0500

    Dynamic url added

--- htdocs/posts/post1.php
+++ htdocs/posts/post1.php
@@ -1,3 +1,5 @@
+<?php include '../config.php'; ?>
+
 <div class="post card-post col-lg-3 col-md-4 col-sm-6 col-xs-12" data-category="category1">
     <!-- Short post view -->
     <div class="card main-blog-card main-blog p-4">
@@ -19,7 +21,7 @@
     <!-- Long post view -->
     <div class="single-post-data pt-3">
         <div class="col-12 first-col">
-            <a href="http://localhost/static-blog-site/blog.php" class="btn btn-link p-0 mb-3 d-flex align-items-center gap-1">
+            <a href="<?= BASE_URL . 'blog.php' ?>" class="btn btn-link p-0 mb-3 d-flex align-items-center gap-1">
                 <span class="d-flex">
                     <svg fill="currentColor" height="13px" width="18px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 242.133 242.133" xml:space="preserve">
                         <path id="XMLID_24_" d="M190.919,212.133h-69.853c-8.284,0-15,6.716-15,15s6.716,15,15,15h106.065c8.284,0,15-6.716,15-15V121.066c0-8.284-6.716-15-15-15s-15,6.716-15,15v69.854L25.607,4.394c-5.858-5.858-15.356-5.858-21.213,0c-5.858,5.858-5.858,15.356,0,21.213L190.919,212.133z"></path>
@@ -27,6 +29,7 @@
                 </span>
                 <span class="ms-1">All posts</span>
             </a>
+
             <h1 class="banner-heading text-dark fs-1 mb-4">Create a Responsive Navigation Bar Using HTML, CSS, and JavaScript</h1>
             <h5 class="post-content-heading text-dark">Description</h5>
             <p class="font-sm text-dark mb-4">In this tutorial, we will walk you through the process of creating a fully responsive navigation bar using HTML, CSS, and JavaScript. A responsive navigation bar ensures that your website provides a seamless experience across various screen sizes, from desktop monitors to mobile phones. By the end of this tutorial, you’ll have a functional, stylish, and adaptable navigation bar that enhances user experience and accessibility.</p>
